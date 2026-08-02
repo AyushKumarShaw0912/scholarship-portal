82ed03902998949bc640850fcfdc467d337e468d
and <ayush.shaw@aerialview6.com>
2026-08-02 13:10:28 +0530

    migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,4 +1,5 @@
 import * as migration_20260802_041617_initial from './20260802_041617_initial';
+import * as migration_20260802_073956_apply_section_personal_family from './20260802_073956_apply_section_personal_family';
 import * as migration_20260802_105000_applications_subject_marks from './20260802_105000_applications_subject_marks';
 import * as migration_20260802_113500_apply_page from './20260802_113500_apply_page';
 import * as migration_20260802_120400_applications_contact_fields from './20260802_120400_applications_contact_fields';
@@ -8,6 +9,11 @@ export const migrations = [
     up: migration_20260802_041617_initial.up,
     down: migration_20260802_041617_initial.down,
     name: '20260802_041617_initial',
+  },
+  {
+    up: migration_20260802_073956_apply_section_personal_family.up,
+    down: migration_20260802_073956_apply_section_personal_family.down,
+    name: '20260802_073956_apply_section_personal_family',
   },
   {
     up: migration_20260802_105000_applications_subject_marks.up,
@@ -22,6 +28,6 @@ export const migrations = [
   {
     up: migration_20260802_120400_applications_contact_fields.up,
     down: migration_20260802_120400_applications_contact_fields.down,
-    name: '20260802_120400_applications_contact_fields',
+    name: '20260802_120400_applications_contact_fields'
   },
 ];
